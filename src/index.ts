f246d37947417121d5461df27419341f13a68cd1
LiosK <contact@mail.liosk.net>
2023-01-07 23:23:58 +0900

    refactor: aggregate ISO 7064 implementations into two classes

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,8 @@ export * from "./algo/luhn.js";
 export * from "./algo/verhoeff.js";
 export * from "./algo/damm.js";
 
-// ISO/IEC 7064:2003, Pure systems
-export * from "./algo/mod11_2.js";
-export * from "./algo/mod37_2.js";
-export * from "./algo/mod97_10.js";
-export * from "./algo/mod661_26.js";
-export * from "./algo/mod1271_36.js";
-
-// ISO/IEC 7064:2003, Hybrid systems
-export * from "./algo/mod11_10.js";
-export * from "./algo/mod27_26.js";
-export * from "./algo/mod37_36.js";
+// ISO/IEC 7064:2003
+export * from "./algo/iso7064.js";
 
 // GTINs (including UPC, EAN, ISBN-13, etc.)
 export * from "./algo/gtin.js";
