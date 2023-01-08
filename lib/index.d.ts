5a1d9984b7d4ac48ec2d0800bd9e6c5057783bc3
LiosK <contact@mail.liosk.net>
2023-01-08 13:32:21 +0900

    build: prepare for release

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,21 +1,14 @@
 /**
  * cdigit
  *
- * @copyright 2018-2022 LiosK
+ * @copyright 2018-2023 LiosK
  * @license (MIT OR Apache-2.0)
  */
-export type { CdigitAlgo } from "./algo/common";
+export type { CdigitAlgo } from "./type";
 export * from "./algo/luhn";
 export * from "./algo/verhoeff";
 export * from "./algo/damm";
-export * from "./algo/mod11_2";
-export * from "./algo/mod37_2";
-export * from "./algo/mod97_10";
-export * from "./algo/mod661_26";
-export * from "./algo/mod1271_36";
-export * from "./algo/mod11_10";
-export * from "./algo/mod27_26";
-export * from "./algo/mod37_36";
+export * from "./algo/iso7064";
 export * from "./algo/gtin";
 /** Supported cdigit names */
 export declare const names: string[];
